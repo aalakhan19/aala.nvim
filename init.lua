636cdebc325a741dad1fa3a76817abed2ca51314
Abdul Aala Khan <abdulaala.khan@stud.h-da.de>
2026-07-31 17:41:46 +0200

    cleanup

--- init.lua
+++ init.lua
@@ -1,47 +1,12 @@
-local path_package = vim.fn.stdpath 'data' .. '/site/'
-local mini_path = path_package .. 'pack/deps/start/mini.deps'
-if not vim.loop.fs_stat(mini_path) then
-	vim.cmd 'echo "Installing `mini.deps`" | redraw'
-	local clone_cmd = {
-		'git',
-		'clone',
-		'--filter=blob:none',
-		'https://github.com/nvim-mini/mini.deps',
-		mini_path,
-	}
-	vim.fn.system(clone_cmd)
-	vim.cmd 'packadd mini.deps | helptags ALL'
-	vim.cmd 'echo "Installed `mini.deps`" | redraw'
-end
-
--- Set up 'mini.deps' (customize to your liking)
-require('mini.deps').setup { path = { package = path_package } }
-
-local add = MiniDeps.add
-
--- Theme
-add 'vague-theme/vague.nvim'
-
-vim.cmd 'colorscheme vague'
-
---options
--- vim.opt.langmap = 'ö[,ä]'
-local function feed(key)
-	return function()
-		local k = vim.api.nvim_replace_termcodes(key, true, false, true)
-		vim.api.nvim_feedkeys(k, 'm', false)
-	end
-end
-
-vim.keymap.set('n', 'ö', feed('['), { desc = '[' })
-vim.keymap.set('n', 'ä', feed(']'), { desc = ']' })
+-- Options
+vim.g.mapleader = ' '
 
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.mouse = 'a'
 vim.opt.showmode = false
 vim.schedule(function()
-	vim.opt.clipboard = 'unnamedplus'
+  vim.opt.clipboard = 'unnamedplus'
 end)
 
 vim.opt.breakindent = true
@@ -67,7 +32,20 @@ vim.opt.smarttab = true
 vim.opt.swapfile = false
 vim.opt.winborder = 'rounded'
 
-vim.g.mapleader = ' '
+vim.opt.foldmethod = 'expr'
+vim.opt.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+vim.opt.foldlevelstart = 99
+
+-- Keymaps
+local function feed(key)
+  return function()
+    local k = vim.api.nvim_replace_termcodes(key, true, false, true)
+    vim.api.nvim_feedkeys(k, 'm', false)
+  end
+end
+
+vim.keymap.set('n', 'ö', feed('['), { desc = '[' })
+vim.keymap.set('n', 'ä', feed(']'), { desc = ']' })
 
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
@@ -82,111 +60,107 @@ vim.keymap.set('n', '<leader>q', ':quit<CR>')
 vim.keymap.set('n', '<leader>F', vim.lsp.buf.format)
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
 
-add 'nmac427/guess-indent.nvim'
-require('guess-indent').setup()
+vim.keymap.set('n', 'gd', vim.lsp.buf.definition)
+vim.keymap.set('n', 'gr', vim.lsp.buf.references)
 
+-- Autocommands
 vim.api.nvim_create_autocmd('TextYankPost', {
-	desc = 'Highlight when yanking (copying) text',
-	group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
 })
 
-add('nvim-mini/mini.icons')
-require('mini.icons').setup()
-
-add 'nvim-mini/mini.surround'
-require('mini.surround').setup()
-
-add 'nvim-mini/mini.ai'
-require('mini.ai').setup {
-	n_lines = 100,
+-- Plugins
+vim.pack.add {
+  'https://github.com/vague-theme/vague.nvim',
+  'https://github.com/nmac427/guess-indent.nvim',
+  'https://github.com/nvim-mini/mini.icons',
+  'https://github.com/nvim-mini/mini.surround',
+  'https://github.com/nvim-mini/mini.ai',
+  'https://github.com/nvim-mini/mini.statusline',
+  'https://github.com/nvim-mini/mini.pairs',
+  'https://github.com/nvim-mini/mini.pick',
+  'https://github.com/nvim-mini/mini.extra',
+  'https://github.com/chomosuke/typst-preview.nvim',
+  'https://github.com/stevearc/oil.nvim',
+  { src = 'https://github.com/nvim-treesitter/nvim-treesitter', version = 'main' },
+  'https://github.com/neovim/nvim-lspconfig',
+  'https://github.com/mason-org/mason.nvim',
 }
 
-add 'nvim-mini/mini.statusline'
+vim.cmd 'colorscheme vague'
+
+require('guess-indent').setup()
+require('mini.icons').setup()
+require('mini.surround').setup()
+require('mini.ai').setup { n_lines = 100 }
 require('mini.statusline').setup()
-
-add 'nvim-mini/mini.pairs'
 require('mini.pairs').setup()
-
-add 'nvim-mini/mini.pick'
 require('mini.pick').setup()
-
-add 'nvim-mini/mini.extra'
 require('mini.extra').setup()
 
-add 'chomosuke/typst-preview.nvim'
-require('typst-preview').setup(
-	{
-		dependencies_bin = { ['tinymist'] = 'tinymist' },
-		open_cmd = 'firefox %s -P typst-preview --class typst-preview'
-	}
-)
+require('typst-preview').setup {
+  dependencies_bin = { ['tinymist'] = 'tinymist' },
+  open_cmd = 'firefox %s -P typst-preview --class typst-preview',
+}
+
+require('oil').setup { default_file_explorer = true }
+vim.keymap.set('n', '-', ':Oil<CR>')
 
 vim.keymap.set('n', '<leader>ff', '<cmd>Pick files<CR>')
 vim.keymap.set('n', '<leader>fg', '<cmd>Pick grep_live<CR>')
 vim.keymap.set('n', '<leader>fb', '<cmd>Pick buffers<CR>')
 vim.keymap.set('n', '<leader>fd', '<cmd>Pick diagnostic<CR>')
+vim.keymap.set('n', '<leader>fD', "<cmd>Pick lsp scope='definition'<CR>")
 vim.keymap.set('n', '<leader>fr', "<cmd>Pick lsp scope='references'<CR>")
-vim.keymap.set('n', '<leader>fd', "<cmd>Pick lsp scope='definition'<CR>")
 
-vim.keymap.set('n', 'gd', vim.lsp.buf.definition)
-vim.keymap.set('n', 'gr', vim.lsp.buf.references)
+-- Treesitter (main branch: highlighting/indent/fold are opt-in per filetype)
+require('nvim-treesitter').install { 'lua', 'vimdoc', 'c_sharp', 'templ', 'rust', 'html' }
 
-add 'stevearc/oil.nvim'
-
-require('oil').setup {
-	default_file_explorer = true,
-}
-vim.keymap.set('n', '-', ':Oil<CR>')
-
-
-add({
-	source = 'nvim-treesitter/nvim-treesitter',
-	checkout = 'master',
-	monitor = 'main',
-	hooks = { post_checkout = function() vim.cmd('TSUpdate') end },
+vim.api.nvim_create_autocmd('PackChanged', {
+  callback = function(ev)
+    if ev.data.spec.name == 'nvim-treesitter' and (ev.data.kind == 'install' or ev.data.kind == 'update') then
+      vim.cmd 'TSUpdate'
+    end
+  end,
 })
 
-require('nvim-treesitter.configs').setup({
-	ensure_installed = { 'lua', 'vimdoc', 'c_sharp', 'templ', 'rust', 'html' },
-	highlight = { enable = true },
-	auto_install = true,
-	indent = { enable = true },
+vim.api.nvim_create_autocmd('FileType', {
+  callback = function(ev)
+    local ok = pcall(vim.treesitter.start, ev.buf)
+    if ok then
+      vim.bo[ev.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+    end
+  end,
 })
 
-vim.opt.foldmethod = "expr"
-vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-vim.opt.foldlevelstart = 99
-
-add({
-	source = 'neovim/nvim-lspconfig',
-	depends = { 'mason-org/mason.nvim' }
-})
-
+-- LSP
 require('mason').setup()
-vim.lsp.enable { 'lua_ls', 'csharp_ls', 'tinymist', 'gopls', 'templ', 'html', 'htmx'}
-vim.diagnostic.config({ virtual_text = true })
+vim.lsp.enable { 'lua_ls', 'csharp_ls', 'tinymist', 'gopls', 'templ', 'html', 'htmx' }
+vim.diagnostic.config { virtual_text = true }
 
-add { source = 'saghen/blink.cmp', checkout = 'v1.7.0' }
-require('blink.cmp').setup {
-	keymap = {
-		preset = 'enter',
-		['<C-l>'] = { 'show', 'show_documentation', 'hide_documentation' },
-	},
-	signature = { enabled = true },
-	completion = { documentation = { auto_show = true } },
-}
+-- Native completion (nvim 0.12+)
+vim.opt.completeopt = 'menuone,noselect,popup'
 
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(ev)
+    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+    if client and client:supports_method 'textDocument/completion' then
+      vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+    end
+  end,
+})
 
+-- Godot integration
 if vim.fn.filereadable(vim.fn.getcwd() .. '/project.godot') == 1 then
-	local addr = './godot.pipe'
-	if vim.fn.has 'win32' == 1 then
-		-- Windows can't pipe so use localhost. Make sure this is configured in Godot
-		-- Exec Path: nvim
-		-- Exec Flags: --server 127.0.0.1:6004 --remote-send "<esc>:n {file}<CR>:call cursor({line},{col})<CR>"
-		addr = '127.0.0.1:6004'
-	end
-	vim.fn.serverstart(addr)
+  local addr = './godot.pipe'
+  if vim.fn.has 'win32' == 1 then
+    -- Windows can't pipe so use localhost. Make sure this is configured in Godot
+    -- Exec Path: nvim
+    -- Exec Flags: --server 127.0.0.1:6004 --remote-send "<esc>:n {file}<CR>:call cursor({line},{col})<CR>"
+    addr = '127.0.0.1:6004'
+  end
+  vim.fn.serverstart(addr)
 end
